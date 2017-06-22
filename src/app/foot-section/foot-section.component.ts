fce0f1ee95a37b9daadcef86b0d88403d4023c3c
jjinha0 <vnfflvdl1@gmail.com>
2017-06-22 18:00:21 +0900

    angular basic

---
+++ src/app/foot-section/foot-section.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+/**
+ * Created by danawacomputer on 2017-06-22.
+ */
+@Component({
+  selector: 'foot-section',
+  templateUrl: './foot-section.component.html'
+})
+export class FootSection{
+
+}
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+/**
+ * Created by danawacomputer on 2017-06-22.
+ */
+@Component({
+  selector: 'main-content',
+  templateUrl: './main-content.component.html'
+})
+export class MainContentComponent{
+
+}
